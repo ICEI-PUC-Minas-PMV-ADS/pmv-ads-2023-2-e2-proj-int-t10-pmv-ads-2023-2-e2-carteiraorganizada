4309f7c90285c3dcab3e6b1372df9279feb639a2
Leander Santos <116657748+LeanderAlm@users.noreply.github.com>
2023-11-05 11:32:16 -0300

    Add files via upload

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C__VSCode.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+   
